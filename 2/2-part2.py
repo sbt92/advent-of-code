fbd3ec90a872fc12cd0a19798c60a09b21a3fdd9
Sam Tripp <1519984+sbt92@users.noreply.github.com>
2022-12-02 15:10:08 +0000

    2022 Day 2

---
+++ 2/2-part2.py
@@ -0,0 +1,30 @@
+rock = 1
+paper = 2
+scissors = 3
+draw = 3
+win = 6
+total = 0
+n = 0
+with open ("input") as f:
+    input = f.readlines()
+    for i in input:
+        if input[n] == "A X\n": 
+            total = total + scissors
+        elif input[n] == "A Y\n": 
+            total = total + rock + draw
+        elif input[n] == "A Z\n": 
+            total = total + paper + win
+        elif input[n] == "B X\n": 
+            total = total + rock
+        elif input[n] == "B Y\n": 
+            total = total + paper + draw
+        elif input[n] == "B Z\n": 
+            total = total + scissors + win       
+        elif input[n] == "C X\n": 
+            total = total + paper
+        elif input[n] == "C Y\n": 
+            total = total + scissors + draw
+        elif input[n] == "C Z\n": 
+            total = total + rock + win
+        n = n + 1
+    print(f'Answer: {total}')
